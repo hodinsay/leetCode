1a8aa4e2880c84d85559a6546d89ad91acfa7a2e
Din S Ho <dinsho@Din-S-M1.local>
2022-10-15 23:02:49 -0700

    Valid Parentheses

---
+++ validParentheses.js
@@ -0,0 +1,52 @@
+// Valid Parentheses
+
+// DESCRIPTION: 
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+ 
+
+// Example 1:
+
+// Input: s = "()"
+// Output: true
+// Example 2:
+
+// Input: s = "()[]{}"
+// Output: true
+// Example 3:
+
+// Input: s = "(]"
+// Output: false
+
+// DIFFICULTY:
+// EASY
+
+// Solution:
+
+function isValid(s) {
+    const stack = [];
+    
+    for (let i = 0 ; i < s.length ; i++) {
+        let c = s.charAt(i);
+        switch(c) {
+            case '(': stack.push(')');
+                break;
+            case '[': stack.push(']');
+                break;
+            case '{': stack.push('}');
+                break;
+            default:
+                if (c !== stack.pop()) {
+                    return false;
+                }
+        }
+    }
+    
+    return stack.length === 0;
+};
