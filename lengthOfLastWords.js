a097f5741d9aad26a38f0f2feeb3133bb5da388a
Din S Ho <dinsho@Din-S-M1.local>
2022-10-24 21:22:42 -0700

    Length of Last Words

---
+++ lengthOfLastWords.js
@@ -0,0 +1,35 @@
+// Length of Last Words
+
+// DESCRIPTION:
+
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+// Example 3:
+
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+
+// DIFFICULTY:
+// EASY
+
+// Solution:
+
+function lengthOfLastWord(s) {
+    const words = s.trim().split(" ");
+    return words.length > 0 ? words[words.length - 1].length : 0;
+};
