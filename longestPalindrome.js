d1f596acc3983dab9c2064371ae54d77559a7c9c
Din S Ho <dinsho@Din-S-M1.local>
2022-11-01 22:49:20 -0700

    Longest Palindromic Substring

---
+++ longestPalindrome.js
@@ -0,0 +1,62 @@
+// Longest Palindromic Substring 
+
+// DESCRIPTION: 
+
+// Given a string s, return the longest palindromic substring in s.
+
+// Example 1:
+
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+// Example 2:
+
+// Input: s = "cbbd"
+// Output: "bb"
+
+// DIFFICULTY:
+// Medium
+
+// Solution: 
+
+function longestPalindrome(string) {
+    let longestPal = '';
+
+    const getLongestPalindrome = function (leftPosition, rightPosition) {
+        // While there is space to expand, and the expanded strings match
+        while (
+            leftPosition >= 0 &&
+            rightPosition < string.length &&
+            string[leftPosition] === string[rightPosition]
+        ) {
+            // Expand in each direction.
+            leftPosition--;
+            rightPosition++;
+        }
+
+        // Store the longest palindrom (if it's the longest one found so far)
+        if (rightPosition - leftPosition > longestPal.length) {
+            longestPal = string.slice(leftPosition + 1, rightPosition);
+        }
+    };
+
+    // Loop through the letters
+    for(let i = 0; i < string.length; i++) {
+        // Find the longest odd palendrome
+        getLongestPalindrome(i, i + 1);
+
+        // Find the longest even palendrome
+        getLongestPalindrome(i, i);
+
+        // Check if a longer palindrome cannot be found
+        if ((string.length - i) * 2 < longestPal.length) {
+            // Break out to avoid unnecessary computation
+            break;
+        }
+    }
+
+    return longestPal;
+
+};
+
+// Source: https://duncan-mcardle.medium.com/leetcode-problem-5-longest-palindromic-substring-javascript-9c7c7cea0003
