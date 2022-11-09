0f237a90411234bc3ddf995de748733b454616d4
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-11-09 09:10:44 -0800

    Remove Element

--- solution/removeElement.js
+++ solution/removeElement.js
@@ -36,6 +36,7 @@
 // Output: 2, nums = [2,2,_,_]
 // Explanation: Your function should return k = 2, with the first two elements of nums being 2.
 // It does not matter what you leave beyond the returned k (hence they are underscores).
+
 // Example 2:
 
 // Input: nums = [0,1,2,2,3,0,4,2], val = 2
