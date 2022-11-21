14ef44c490011d41b68709ea09d65a14fe430dd1
Din S Ho <dinsho@Din-S-M1.local>
2022-11-20 22:41:33 -0800

    Generate Parenthesis

---
+++ solution/generateParenthesis.js
@@ -0,0 +1,63 @@
+// Generate Parentheses
+
+// DIFFICULTY: Medium 
+
+// DESCRIPTION: 
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ 
+// Example 1:
+
+// Input: n = 3
+// Output: ["((()))","(()())","(())()","()(())","()()()"]
+// Example 2:
+
+// Input: n = 1
+// Output: ["()"]
+
+// SOLUTION:
+
+const generateParenthesis = function(n) {
+    let optionsArray = [];
+
+    function innerGenerateParentheses(
+        currentString,
+        openBrackets,
+        closedBrackets,
+        maximumPairs
+    ) {
+        // If the current string is full
+        if (currentString.length == maximumPairs * 2) {
+            // Add it to the list of possibilities
+            optionsArray.push(currentString);
+            return;
+        }
+
+        // If more brackets can be opened
+        if (openBrackets < maximumPairs) {
+            // Call the function again, with one additional bracket opened
+            innerGenerateParentheses(
+                currentString + '(',
+                openBrackets + 1,
+                closedBrackets,
+                maximumPairs
+            );
+        }
+
+        // If open brackets still need to be closed
+        if (closedBrackets < openBrackets) {
+            // Call the function again, with one additional bracket closed
+            innerGenerateParentheses(
+                currentString + ')',
+                openBrackets,
+                closedBrackets + 1,
+                maximumPairs
+            );
+        }
+    }
+
+    // Recursively obtain all possibilties
+    innerGenerateParentheses('', 0, 0, n);
+    return optionsArray;
+};
+
+// Source: https://duncan-mcardle.medium.com/leetcode-problem-23-generate-parentheses-javascript-f7264d798a1c
