320cc26cc5ac5676746854fe24511f7f48cde2f1
Din S Ho <dinsho@Din-S-M1.local>
2022-11-22 23:16:05 -0800

    Fizzbuzz

--- solution/fizzBuzz.js
+++ solution/fizzBuzz.js
@@ -1,5 +1,9 @@
 // FizzBuzz
 
+// LANGUAGE: JavaScript
+
+// DIFFICULTY: EASY
+
 // DESCRIPTION: 
 
 // Given an integer n, return a string array answer (1-indexed) where:
@@ -8,31 +12,35 @@
 // answer[i] == "Fizz" if i is divisible by 3.
 // answer[i] == "Buzz" if i is divisible by 5.
 // answer[i] == i (as a string) if none of the above conditions are true.
- 
 
 // Example 1:
 
 // Input: n = 3
 // Output: ["1","2","Fizz"]
+
 // Example 2:
 
 // Input: n = 5
 // Output: ["1","2","Fizz","4","Buzz"]
+
 // Example 3:
 
 // Input: n = 15
 // Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
- 
-// Dificulty:
-// Easy
 
 // Solution:
 
+// Frequency Counters Pattern: Using [] to collect values/frequencies of values. 
+
 function fizzBuzz(n) {
+    // Set variable with array [] to collect values.
     let arr = [];
     
+    // Create a For loop for n input. 
     for (let i = 1; i <= n; i++) {
+    // Create if ... else conditions. Use % operation to determine odd n value.
         if (i % 3 === 0 && i % 5 === 0) {
+    // Use push() method to add value to the end of the array list. 
             arr.push('FizzBuzz');
         }
         else if (i % 3 === 0) {
@@ -42,10 +50,18 @@ function fizzBuzz(n) {
             arr.push('Buzz');
         }
         else {
+    // Use toString() method to push n input to the array list as string value if not . 
             arr.push(i.toString());
         }
     }
+
+    // Return array list as the output.
     return arr;
 };
 
-// Source: https://duncan-mcardle.medium.com/leetcode-problem-412-fizzbuzz-javascript-3944df2d6475
+// Runtime: ~125ms
+// Memory: ~44.1 MB
+
+// Source: 
+// https://duncan-mcardle.medium.com/leetcode-problem-412-fizzbuzz-javascript-3944df2d6475
+// https://leetcode.com/problems/fizz-buzz/submissions/
